d2723ecfbdb6aa36161d9eb3a3d22c19405e8904
romel <romelmahmud.dev@gmail.com>
2024-06-04 00:11:40 +0600

    student validation zod schema add

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,22 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { AnyZodObject } from 'zod';
+import { studentValidationSchemas } from '../student/student.validation';
 import { UserControllers } from './user.controller';
 
 const router = express.Router();
 
-router.post('/create-student', UserControllers.createStudent);
+const validateRequest = (schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const zodParsedData = await schema.parseAsync({
+      body: req.body,
+    });
+  };
+};
+
+router.post(
+  '/create-student',
+  validateRequest(studentValidationSchemas.studentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
