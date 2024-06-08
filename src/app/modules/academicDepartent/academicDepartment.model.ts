ce2c8b5c9b4fadda27fbecb0ecb1d44cd52af124
romel <romelmahmud.dev@gmail.com>
2024-06-09 00:14:19 +0600

    checking if academic department exists

--- src/app/modules/academicDepartent/academicDepartment.model.ts
+++ src/app/modules/academicDepartent/academicDepartment.model.ts
@@ -13,6 +13,18 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>({
   },
 });
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error('this department already exist');
+  }
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
