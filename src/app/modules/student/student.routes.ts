d6da7c20f12e773285db8634c44a66947ad9097d
romel <romelmahmud.dev@gmail.com>
2024-06-10 03:29:37 +0600

    implete delete user studetn using transaction and rollback

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -4,5 +4,7 @@ import { StudentControllers } from './student.controller';
 const router: Router = express.Router();
 
 router.get('/', StudentControllers.getAllStudents);
+router.get('/:studentId', StudentControllers.getSingleStudent);
+router.delete('/:studentId', StudentControllers.deleteStudent);
 
 export const StudentRoutes = router;
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,5 +1,7 @@
 import httpStatus from 'http-status';
+import mongoose from 'mongoose';
 import { AppError } from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { IStudent } from './student.interface';
 import { Student } from './student.model';
 
@@ -37,8 +39,42 @@ const getSingleStudentFromDB = async (id: string) => {
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+  if (!(await Student.isUserExists(id))) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student does not exists!');
+  }
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
+    }
+
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session },
+    );
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+
+    return deletedStudent;
+  } catch (error: any) {
+    await session.abortTransaction();
+    await session.endSession();
+
+    throw new Error(error);
+  }
 };
 
 export const StudentServices = {
